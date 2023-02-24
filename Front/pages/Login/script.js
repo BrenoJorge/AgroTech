4fbc74378c4d6d7a768f7f4452a98481e5d2d65c
BrenoJorge <brenojorge79@gmail.com>
2023-02-24 11:26:46 -0300

    2/24 fri am

---
+++ Front/pages/Login/script.js
@@ -0,0 +1,33 @@
+const form = document.querySelector("form")
+const email = document.querySelector("#email")
+const senha = document.querySelector("#senha")
+const mensagem = document.querySelector(".mensagem")
+
+function fazerLogin() {
+
+    let dados = {
+        "email": email.value,
+        "senha": senha.value
+    }
+
+    const options = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(dados)
+      };
+      
+      fetch('http://localhost:3000/login', options)
+        .then(response => response.json())
+        .then(response => {
+            if(response.mensagem == "Usuario não encontrado"){
+                mensagem.classList.remove("model")
+                mensagem.querySelector("h3").innerHTML = response.mensagem
+            }
+        })
+        .catch(err => console.error(err));
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    fazerLogin()
+})
